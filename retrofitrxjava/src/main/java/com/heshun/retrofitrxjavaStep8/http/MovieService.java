2975575b74d5c9908cd88068dbe6485af7d7da0b
jcs <jichensheng@foxmail.com>
2017-04-26 17:03:29 +0800

    retrofit

---
+++ retrofitrxjava/src/main/java/com/heshun/retrofitrxjavaStep8/http/MovieService.java
@@ -0,0 +1,28 @@
+package com.heshun.retrofitrxjavaStep8.http;
+
+import com.heshun.retrofitrxjavaStep8.entity.HttpResult;
+import com.heshun.retrofitrxjavaStep8.entity.Subject;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import rx.Observable;
+
+/**
+ * Created by liukun on 16/3/9.
+ */
+public interface MovieService {
+
+//    @GET("top250")
+//    Call<MovieEntity> getTopMovie(@Query("start") int start, @Query("count") int count);
+
+//    @GET("top250")
+//    Observable<MovieEntity> getTopMovie(@Query("start") int start, @Query("count") int count);
+
+//    @GET("top250")
+//    Observable<HttpResult<List<Subject>>> getTopMovie(@Query("start") int start, @Query("count") int count);
+
+    @GET("top250")
+    Observable<HttpResult<List<Subject>>> getTopMovie(@Query("start") int start, @Query("count") int count);
+}
