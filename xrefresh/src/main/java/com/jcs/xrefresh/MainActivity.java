aae95c41ddad4544be5d94edf77c746083415ef1
jcs <jichensheng@foxmail.com>
2017-08-15 10:07:14 +0800

    xrecycler

--- xrefresh/src/main/java/com/jcs/xrefresh/MainActivity.java
+++ xrefresh/src/main/java/com/jcs/xrefresh/MainActivity.java
@@ -18,6 +18,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * xrv测试
+ */
 public class MainActivity extends AppCompatActivity {
 	private XRecyclerView mRecyclerView;
 	private int refreshTime = 0;
